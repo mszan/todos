138d51e0b320d0fa600ad51f8bef6b5b3cbba639
Dawid Mszanowski <dmszanowski@icloud.com>
2020-12-29 00:50:11 +0100

    Update Profile.jsx to fetch user data

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const app = express()
 const pool = require("../../db")
-const bcrypt = require("bcrypt")
 
 // Gets all users.
 app.get("/", async (req, res) => {
   try {
-    const getUsers = await pool.query("SELECT id, username, firstname, lastname, active, registerDate FROM users")
-    res.json(getUsers[0])
+    const { username } = req.query
+    if (username) {
+      if (username.split(" ").length > 1) {
+        return res.status(400).json({"msg": "Wrong 'username' value."})
+      }
+    }
+
+    let query = "SELECT id, username, firstname, lastname, active, registerDate FROM users"
+    username ? query += ` WHERE username = '${username}'` : null
+
+    console.log(query)
+
+    pool.query(query)
+        .then(queryRes => {
+          res.json(queryRes[0])
+        })
+
   } catch(err) {
     console.error(err.message)
   }
--- frontend/src/components/Users/Profile.jsx
+++ frontend/src/components/Users/Profile.jsx
@@ -1,31 +1,65 @@
 import React from 'react';
-import {Col, Row, Skeleton} from "antd";
+import {Col, Divider, Row, Skeleton} from "antd";
 import Avatar from "antd/es/avatar/avatar";
 import {UserOutlined} from "@ant-design/icons";
 import QueueAnim from 'rc-queue-anim';
 import Title from "antd/es/typography/Title";
+import axios from "axios";
+import authHeader from "../../services/auth-header";
+import Paragraph from "antd/es/typography/Paragraph";
+import moment from "moment";
 
 // User profile page
 export class Profile extends React.Component {
-    state = {
-        username: localStorage.getItem("username")
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {
+                username: "",
+                firstname: "",
+                lastname: "",
+                registerDate: ""
+            }
+        }
+    }
+
+    componentDidMount() {
+        this.fetchUser(localStorage.getItem("username"))
+    }
+
+    // Fetch user information from server
+    fetchUser = username => {
+        // Send request
+        axios.get(process.env.REACT_APP_API_URL + `users?username=${username}`, {headers: authHeader()})
+            .then(response => {
+                // Set state with user data from response
+                this.setState({
+                    user: response.data[0]
+                })
+            })
+            // Catch errors
+            .catch(err => console.log(err))
+    }
 
     render() {
-        const {username} = this.state
 
         return (
             <React.Fragment>
-
                 <Row gutter={16}>
                     <QueueAnim component={Col} type="scaleY">
                         <Avatar key="avatar" shape="square" size={128} icon={<UserOutlined/>}/>
                     </QueueAnim>
                     <QueueAnim component={Col} type="scaleY">
-                        {username ? <Title key="username"> {username} </Title> : <Skeleton/>}
+                        {this.state.user.username ? <Title key="username" level={1}> {this.state.user.username} </Title> : null}
+                        {this.state.user.firstname && this.state.user.lastname ? <Title key="fullName" level={5}> {this.state.user.firstname} {this.state.user.lastname} </Title> : null}
+                        {this.state.user.registerDate ? <Paragraph key="registerDate"> Joined {moment(this.state.user.registerDate).fromNow()} </Paragraph> : null}
                     </QueueAnim>
                 </Row>
-
+                <Row gutter={16}>
+                    <QueueAnim component={Col} componentProps={{span: 24}} type="scaleY">
+                        <Divider key="headerDivider"/>
+                    </QueueAnim>
+                </Row>
             </React.Fragment>
         );
     }
