451cf55ce4d85306daa3ca621f8db05913203a1f
Dawid Mszanowski <dmszanowski@icloud.com>
2020-12-31 00:47:17 +0100

    Add comments

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,17 +5,19 @@ const pool = require("../../db")
 // Gets all users.
 app.get("/", async (req, res) => {
   try {
+    // Get data from request params.
     const { username } = req.query
+
+    // If username is set, check if it has more than 1 word
     if (username) {
       if (username.split(" ").length > 1) {
         return res.status(400).json({"msg": "Wrong 'username' value."})
       }
     }
 
+    // Query string.
     let query = "SELECT id, username, firstname, lastname, active, registerDate FROM users"
     username ? query += ` WHERE username = '${username}'` : null
-
-    console.log(query)
 
     pool.query(query)
         .then(queryRes => {
